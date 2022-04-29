485b6cadeb82dc917eab3184eb2affc11c8ac24d
Oong2 <ehddns0528@gmail.com>
2022-04-29 15:07:27 +0900

    220429

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-    
+    $(".top-wrap > .top-menu-wrap > .top-right > .btn-menu").click(function(){
+       
+        let index = $(this)
+        
+        $(".menubar-wrap").toggleClass("active");
+
+        $(".top-wrap > .top-menu-wrap > .top-right > .btn-menu > .btn2").addClass("active")
+        $(".top-wrap > .top-menu-wrap > .top-right > .btn-menu > .btn").addClass("active")
+        $(".menubar-wrap").addClass("active")
+
+        if(
+            $(".top-wrap > .top-menu-wrap > .top-right > .btn-menu > .btn2").hasClass("active") ){ 
+                $(".top-wrap > .top-menu-wrap > .top-right > .btn-menu").click(function(){
+                $(".top-wrap > .top-menu-wrap > .top-right > .btn-menu > .btn").removeClass("active")
+            })
+        }
+        if(
+            $(".top-wrap > .top-menu-wrap > .top-right > .btn-menu > .btn").hasClass("active") ){ 
+                $(".top-wrap > .top-menu-wrap > .top-right > .btn-menu").click(function(){
+                $(".top-wrap > .top-menu-wrap > .top-right > .btn-menu > .btn2").removeClass("active")
+            })
+        }
+        
+
+    })
 })
